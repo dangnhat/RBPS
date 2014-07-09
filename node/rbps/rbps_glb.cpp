c05c1270552d4d8af047ebaff77d341ecd18d5bf
Pham Huu Dang Nhat <phamhuudangnhat@gmail.com>
2014-07-09 23:52:17 +0700

    fixed rbps_ns

--- node/rbps/rbps_glb.cpp
+++ node/rbps/rbps_glb.cpp
@@ -19,23 +19,29 @@ rbpm_gui rbps_node_gui;
 ISRMgr_ns::ISR_t keypad_timer = ISRMgr_ns::ISRMgr_TIM6;
 
 /* shared queue id */
-OS_EventID main2znp_queue_id;
-OS_EventID znp2main_queue_id;
+namespace rbps_ns {
+	OS_EventID main2znp_queue_id;
+	OS_EventID znp2main_queue_id;
 
-static const uint8_t main2znp_queue_size = 128;
-static const uint8_t znp2main_queue_size = 128;
+	static const uint8_t main2znp_queue_size = 128;
+	static const uint8_t znp2main_queue_size = 128;
 
-static void *main2znp_queue[main2znp_queue_size];
-static void *znp2main_queue[znp2main_queue_size];
+	static void *main2znp_queue[main2znp_queue_size];
+	static void *znp2main_queue[znp2main_queue_size];
 
-cir_queue main2znp_cir_queue;
-cir_queue znp2main_cir_queue;
+	cir_queue main2znp_cir_queue;
+	cir_queue znp2main_cir_queue;
+}
+
+using namespace rbps_ns;
 /*----------------------------------------------------------------------------*/
 void rbps_init(void) {
 	/* assign ISR for keypad */
 	MB1_ISRs.subISR_assign(keypad_timer, keypad_timerISR);
 
 	/* init queue */
-	main2znp_queue_id = CoCreateQueue(main2znp_queue, main2znp_queue_size, EVENT_SORT_TYPE_FIFO);
-	znp2main_queue_id = CoCreateQueue(znp2main_queue, znp2main_queue_size, EVENT_SORT_TYPE_FIFO);
+	main2znp_queue_id = CoCreateQueue(main2znp_queue,
+			main2znp_queue_size, EVENT_SORT_TYPE_FIFO);
+	znp2main_queue_id = CoCreateQueue(znp2main_queue,
+			znp2main_queue_size, EVENT_SORT_TYPE_FIFO);
 }
