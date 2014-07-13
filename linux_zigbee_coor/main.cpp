3cd37bf8436a43e502b8c24ea311fa0a727c738f
Pham Huu Dang Nhat <phamhuudangnhat@gmail.com>
2014-07-13 18:15:05 +0700

    initial controller thread

--- linux_zigbee_coor/main.cpp
+++ linux_zigbee_coor/main.cpp
@@ -32,20 +32,8 @@ int main() {
 		RBPS_PRINTF("main_thread:Can't create thread\n");
 	}
 
-	/* wait for message */
-	while (1) {
-		ret_val = msgrcv(rbps_ns::zb2cc_mq_id, &a_mesg, rbps_ns::mtext_max_size, 0, IPC_NOWAIT);
-		if (ret_val > 0) {
-			RBPS_PRINTF("main_thread:New message from zigbee thread\n");
-			RBPS_PRINTF("main_thread:SrcAddr %x\n", (uint16_t)a_mesg.mtype);
-			RBPS_PRINTF("main_thread:len %d, cmd %x\n",
-					a_mesg.mtext[0], a_mesg.mtext[1] | (a_mesg.mtext[2] << 8));
-			for (count = 0; count < a_mesg.mtext[0]; count++) {
-				RBPS_PRINTF("%x ", (uint8_t)a_mesg.mtext[count+3]);
-			}
-			RBPS_PRINTF("\n");
-		}
-	}
+	/* start controller thread */
+	controller_thread_func(NULL);
 
 	return 0;
 }
